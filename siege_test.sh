a96683a090392601eb733b299426d0c5fb8a56a5
agaley <agaley@student.42lyon.fr>
2024-07-17 15:46:49 +0200

    [Test] Improve make siege and tests

--- siege_test.sh
+++ siege_test.sh
@@ -1,51 +1,75 @@
 #!/bin/bash
 
-SIEGE_URLS_FILE="siegeurls.txt"
+SIEGE_CGI_URLS_FILE="urls_cgi.txt"
+SIEGE_GET_URLS_FILE="urls_get.txt"
 SIEGE_LOG_FILE="siege.log"
+TEST_DURATION=30s
 TEST_FAILED=0
 
-cat <<EOL > $SIEGE_URLS_FILE
-http://${CONTAINER}:${PORT}/
-http://${CONTAINER}:8090/
-http://${CONTAINER}:8091/
-http://${CONTAINER}:8092/
-http://${CONTAINER}:8093/
-http://${CONTAINER}:${PORT}/nonexistent
+cat <<EOL > $SIEGE_CGI_URLS_FILE
 http://${CONTAINER}:${PORT}/cgi/hello.py?name=Marvin
+http://${CONTAINER}:${PORT}/cgi/hello.php?name=Marvin
 http://${CONTAINER}:${PORT}/cgi/hello.js?name=Marvin
 http://${CONTAINER}:${PORT}/cgi/post.py POST {"name": "Marvin"}
 http://${CONTAINER}:${PORT}/cgi/post.php POST {"name": "Marvin"}
 http://${CONTAINER}:${PORT}/cgi/post.js POST {"name": "Marvin"}
 EOL
 
-cat $SIEGE_URLS_FILE > $SIEGE_LOG_FILE
+cat <<EOF > $SIEGE_GET_URLS_FILE
+http://${CONTAINER}:${PORT}/
+http://${CONTAINER}:8090/
+http://${CONTAINER}:8091/
+http://${CONTAINER}:8092/
+http://${CONTAINER}:8093/
+EOF
+
+echo "CGI urls :"
+cat $SIEGE_CGI_URLS_FILE > $SIEGE_LOG_FILE
 printf "\n" >> $SIEGE_LOG_FILE
+echo "GET urls :"
+cat $SIEGE_GET_URLS_FILE >> $SIEGE_LOG_FILE
+printf "\n" >> $SIEGE_LOG_FILE
+
 
 siege > /dev/null 2>&1
 
-echo "Running siege with no-cache"
-echo "Running siege with no-cache" >> $SIEGE_LOG_FILE
-siege -t 10s -c 250 -b -f $SIEGE_URLS_FILE -H "Cache-Control: no-cache" >> $SIEGE_LOG_FILE
+echo "Running siege with no-cache for CGIs"
+echo "Running siege with no-cache for CGIs" >> $SIEGE_LOG_FILE
+siege -t ${TEST_DURATION} -c 250 -b -f $SIEGE_CGI_URLS_FILE -H "Cache-Control: no-cache" >> $SIEGE_LOG_FILE
 
 if grep -q '"failed_transactions": *[1-9]' $SIEGE_LOG_FILE; then
-    echo "!!!KO!!!: Siege test failed with no-cache. Check $SIEGE_LOG_FILE for details."
+    echo "!!!KO!!!: Siege test failed with no-cache for CGIs. Check $SIEGE_LOG_FILE for details."
     TEST_FAILED=1
 else
-    echo "OK: Siege test passed with no-cache."
+    echo "OK: Siege test passed with no-cache for CGIs."
 fi
 printf "\n"
 
 sleep 2
 
-echo "Running siege with caching enabled"
-echo "Running siege with caching enabled" >> $SIEGE_LOG_FILE
-siege -t 10s -c 250 -b -f $SIEGE_URLS_FILE >> $SIEGE_LOG_FILE
+echo "Running siege with caching enabled for CGIs"
+echo "Running siege with caching enabled for CGIs" >> $SIEGE_LOG_FILE
+siege -t ${TEST_DURATION} -c 250 -b -f $SIEGE_CGI_URLS_FILE -H "Cookie: sessionid=marvin;" >> $SIEGE_LOG_FILE
 
 if grep -q '"failed_transactions": *[1-9]' $SIEGE_LOG_FILE; then
-    echo "!!!KO!!!: Siege test failed with caching enabled. Check $SIEGE_LOG_FILE for details."
+    echo "!!!KO!!!: Siege test failed with caching enabled for CGIs. Check $SIEGE_LOG_FILE for details."
     TEST_FAILED=1
 else
-    echo "OK: Siege test passed with caching enabled."
+    echo "OK: Siege test passed with caching enabled for CGIs."
+fi
+printf "\n"
+
+sleep 2
+
+echo "Running siege with caching enabled for GET"
+echo "Running siege with caching enabled for GET" >> $SIEGE_LOG_FILE
+siege -t ${TEST_DURATION} -c 250 -b -f $SIEGE_GET_URLS_FILE -H "Cookie: sessionid=marvin;" >> $SIEGE_LOG_FILE
+
+if grep -q '"failed_transactions": *[1-9]' $SIEGE_LOG_FILE; then
+    echo "!!!KO!!!: Siege test failed with caching enabled for GET. Check $SIEGE_LOG_FILE for details."
+    TEST_FAILED=1
+else
+    echo "OK: Siege test passed with caching enabled for GET."
 fi
 printf "\n"
 
